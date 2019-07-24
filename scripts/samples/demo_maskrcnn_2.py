49086291186a2b223aef11cbf7a566e2a6c053f7
wuzida <501215134@qq.com>
2019-07-24 19:18:34 +0800

    maskrcnn online process image and publish topic success

--- scripts/samples/demo_maskrcnn_2.py
+++ scripts/samples/demo_maskrcnn_2.py
@@ -49,16 +49,29 @@ def display_instances(image,boxes,masks,ids,names,scores):
         if not np.any(boxes[i]):
             continue
         
-        y1,x1,y2,x2=boxes[i]
-        mask=masks[:,:,i]
-        image=apply_mask(image,mask,color)
-        image=cv2.rectangle(image,(x1,y1),(x2,y2),color,2)
+ #       y1,x1,y2,x2=boxes[i]
+ #       mask=masks[:,:,i]
+ #       image=apply_mask(image,mask,color)
+ #       image=cv2.rectangle(image,(x1,y1),(x2,y2),color,2)
         
+ #       label=names[ids[i]]
+ #       score=scores[i] if scores is not None else None
+        
+ #       caption='{}{:.2f}'.format(label,score) if score else label
+ #       image=cv2.putText(
+  #          image,caption,(x1,y1),cv2.FONT_HERSHEY_COMPLEX,0.7,color,2
+#        )
         label=names[ids[i]]
-        score=scores[i] if scores is not None else None
+        if label == 'keyboard':
+            y1,x1,y2,x2=boxes[i]
+            mask=masks[:,:,i]
+            image=apply_mask(image,mask,color)
+            image=cv2.rectangle(image,(x1,y1),(x2,y2),color,2)
         
-        caption='{}{:.2f}'.format(label,score) if score else label
-        image=cv2.putText(
+            score=scores[i] if scores is not None else None
+        
+            caption='{}{:.2f}'.format(label,score) if score else label
+            image=cv2.putText(
             image,caption,(x1,y1),cv2.FONT_HERSHEY_COMPLEX,0.7,color,2
         )
         
@@ -70,14 +83,14 @@ class image_converter:
     def __init__(self):
         self.image_sub = rospy.Subscriber("/RGB_image",Image,self.callback,queue_size=1)
         self.image_pub = rospy.Publisher("/maskrcnn_image",Image,queue_size=1)
-        ROOT_DIR = os.path.abspath("")
+        ROOT_DIR = os.path.abspath("") # this path is not the .py path, its the command terminal path!
         sys.path.append(ROOT_DIR)
  
         sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
 
         MODEL_DIR = os.path.join(ROOT_DIR, "logs")
-        #COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
-        COCO_MODEL_PATH = ("/media/tx2/Drive/wuzida/catkin_ws/src/maskRCNN/scripts/mask_rcnn_coco.h5")
+        COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
         if not os.path.exists(COCO_MODEL_PATH):
             print('cannot find coco_model')
             utils.download_trained_weights(COCO_MODEL_PATH)
@@ -91,13 +104,13 @@ class image_converter:
         config = InferenceConfig()
         config.display()
     
-        #model = modellib.MaskRCNN(
-       #     mode="inference", model_dir=MODEL_DIR, config=config
-       # )
+        model = modellib.MaskRCNN(
+            mode="inference", model_dir=MODEL_DIR, config=config
+        )
         print("rcnn network ready")
  
         # Load weights trained on MS-COCO
-       # model.load_weights(COCO_MODEL_PATH, by_name=True)
+        model.load_weights(COCO_MODEL_PATH, by_name=True)
         print("rcnn weights load ready")
         class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
                'bus', 'train', 'truck', 'boat', 'traffic light',
@@ -115,15 +128,26 @@ class image_converter:
                'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
                'teddy bear', 'hair drier', 'toothbrush']
         bridge = CvBridge()
-        print (next(os.walk(IMAGE_DIR))[2])
-        file_names = next(os.walk(IMAGE_DIR))[2]
- #       for i in range(len(file_names)):
-        cv_image = skimage.io.imread(os.path.join(IMAGE_DIR, file_names[0]))
-        msg = bridge.cv2_to_imgmsg(cv_image,encoding = "bgr8")
+        file_names = os.listdir(IMAGE_DIR)
+
         rate = rospy.Rate(5)
         while not rospy.is_shutdown():
-            self.image_pub.publish(msg)
-            rate.sleep()
+    
+            for i in range(len(file_names)):
+                start = time.time()
+                cv_image = skimage.io.imread(os.path.join(IMAGE_DIR, file_names[i]))
+
+                results=model.detect([cv_image],verbose=1)
+                r=results[0]
+
+                cv_image=display_instances(
+                    cv_image,r['rois'], r['masks'], r['class_ids'], class_names, r['scores']
+                )
+                msg = bridge.cv2_to_imgmsg(cv_image,encoding = "bgr8")
+                elapsed =(time.time() - start)
+                print ("TIME USED ", elapsed)
+                self.image_pub.publish(msg)
+                rate.sleep()
 
     def callback(self,ros_data):
         try:
