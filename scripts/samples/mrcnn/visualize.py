b605d5eb95b5c9ffbff6a05dd817616e3dfb13fb
wuzida <501215134@qq.com>
2019-08-05 09:31:42 +0800

    recover the original model of visulize

--- scripts/samples/mrcnn/visualize.py
+++ scripts/samples/mrcnn/visualize.py
@@ -73,13 +73,13 @@ def apply_mask(image, mask, color, alpha=0.5):
     """Apply the given mask to the image.
     """
     for c in range(3):
-#        image[:, :, c] = np.where(mask == 1,
-#                                  image[:, :, c] *
-#                                  (1 - alpha) + alpha * color[c] * 255,
-#                                  image[:, :, c])
         image[:, :, c] = np.where(mask == 1,
-                                  image[:, :, c],
-                                  [255, 255, 255])
+                                  image[:, :, c] *
+                                  (1 - alpha) + alpha * color[c] * 255,
+                                  image[:, :, c])
+ #       image[:, :, c] = np.where(mask == 1,
+#                                  image[:, :, c],
+ #                                 [255, 255, 255])
     return image
 
 
